30a5e7ef84311670eccfe778f990fce2ff72ba34
James Moseti <moseti.james4@gmail.com>
2025-09-28 22:19:56 +0300

    fixt: add redirect to Swagger UI in URL configuration

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -43,4 +44,6 @@ urlpatterns = [
     path('api/orders/', include('orders.urls')),
     path('api/products/', include('products.urls')),
     path("api/payments/", include("payments.urls")),
+
+    path("", lambda request: redirect("swagger-ui", permanent=False)),
 ]
